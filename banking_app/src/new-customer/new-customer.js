3fa7e6399625fae071e310b4c4fa4bd034f0687c
xhresuru <surajshrestha690690@gmail.com>
2024-06-08 15:00:42 +0545

    Worked on New-Customer page

--- banking_app/src/new-customer/new-customer.js
+++ banking_app/src/new-customer/new-customer.js
@@ -1,18 +1,29 @@
+import styles from "./new-customer.module.css";
+
 export function NewCustomer() {
+  const onNewCustomer = (e) => {
+    e.preventDefault();
+
+    console.log(e.target);
+    const acId = e.target.acId.value;
+    const acName = e.target.acName.value;
+    const balance = e.target.balance.value;
+
+    console.log(`Id ${acId} Name ${acName} Balance ${balance}`);
+  };
+
   return (
-    <>
+    <div className={styles.custCont}>
       <h1> Create New Customer</h1>
-      <form>
-        <div>
-          <input type="number" placeholder="Account Id" />
-        </div>
-        <div>
-          <input type="text" placeholder="Account Name" />
-        </div>
-        <div>
-          <input type="number" placeholder="Balance" />
-        </div>
+      <form onSubmit={onNewCustomer}>
+        <input type="number" placeholder="Account Id" name="acId" />
+
+        <input type="text" placeholder="Account Name" name="acName" />
+
+        <input type="number" placeholder="Balance" name="balance" />
+
+        <input type="submit" value="Create" />
       </form>
-    </>
+    </div>
   );
 }
