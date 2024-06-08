e3bf4638ba78c9d65a9dac11e51697ad7bd12fa1
xhresuru <surajshrestha690690@gmail.com>
2024-06-08 14:33:04 +0545

    Worked on creating new customer page

--- banking_app/src/dashboard/dashboard.js
+++ banking_app/src/dashboard/dashboard.js
@@ -1,9 +1,12 @@
 import styles from "./dashboard.module.css";
+import { useNavigate } from "react-router-dom";
 
 export function Dashboard() {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.dashCont}>
-      <div>New Customer</div>
+      <div onClick={() => navigate("/new")}>New Customer</div>
       <div>Deposit</div>
       <div>Withdraw</div>
       <div>Transfer</div>
--- banking_app/src/new-customer/new-customer.js
+++ banking_app/src/new-customer/new-customer.js
@@ -1,3 +1,18 @@
 export function NewCustomer() {
-  return <>This is NewCustomer</>;
+  return (
+    <>
+      <h1> Create New Customer</h1>
+      <form>
+        <div>
+          <input type="number" placeholder="Account Id" />
+        </div>
+        <div>
+          <input type="text" placeholder="Account Name" />
+        </div>
+        <div>
+          <input type="number" placeholder="Balance" />
+        </div>
+      </form>
+    </>
+  );
 }
