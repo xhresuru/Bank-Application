6a7f191291a363f361e27fef4db36f48c1db8db9
xhresuru <surajshrestha690690@gmail.com>
2024-06-08 14:20:37 +0545

    Worked on Dashboard Page

--- banking_app/src/dashboard/dashboard.js
+++ banking_app/src/dashboard/dashboard.js
@@ -1,3 +1,13 @@
+import styles from "./dashboard.module.css";
+
 export function Dashboard() {
-  return <>This is Dashboard</>;
+  return (
+    <div className={styles.dashCont}>
+      <div>New Customer</div>
+      <div>Deposit</div>
+      <div>Withdraw</div>
+      <div>Transfer</div>
+      <div>Balance</div>
+    </div>
+  );
 }
--- banking_app/src/index.js
+++ banking_app/src/index.js
@@ -1,3 +1,4 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
